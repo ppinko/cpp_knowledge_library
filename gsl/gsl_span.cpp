0238476a9400c7ae2c944f3950e10d6d755a1ef1
ppinko <p.pinkowicz@gmail.com>
2020-09-07 20:17:07 +0200

    Developing notes about GSL library.

--- gsl/gsl_span.cpp
+++ gsl/gsl_span.cpp
@@ -1,3 +1,19 @@
+/*
+great post - https://stackoverflow.com/questions/45723819/what-is-a-span-and-when-should-i-use-one
+
+source - https://www.modernescpp.com/index.php/c-core-guideline-the-guidelines-support-library
+
+A view is never an owner. In the case of a gsl::span<T> it represents a non-owning 
+range of continguous memory. This can be an array, a pointer with a size, or a 
+std::vector. The same holds for gsl::string_span<T> or a zero terminated C strings: 
+gsl::(cw)zstring.The main reason for having a gsl::span<T> is that a plain array 
+will be decayed to a pointer if passed to a function; therefore the size is lost.
+
+gsl::span<T> automatically deduces the size of the plain array or the std::vector. 
+If you use a pointer, you have to provide the size.
+*/
+
+
 #include <iostream>
 #include <gsl/gsl>
 
